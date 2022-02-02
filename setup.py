56339e0d942df522e40d20fca23701c188fc7baf
Artyom Zaporozhets <kraysent@gmail.com>
2022-02-02 12:28:55 +0300

    fix compile errors

--- setup.py
+++ setup.py
@@ -14,4 +14,15 @@ setup(
     packages=['physical_evaluator'],
     zip_safe=False,
     test_suite='physical_evaluator.tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
